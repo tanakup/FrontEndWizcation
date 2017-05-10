96c72c17403a08d94cf53346786d108d6aab4dda
tanakup <tanakup.psu@gmail.com>
2017-05-10 22:19:14 +0700

    query json and show on site

--- FrontEndWizcation/scripts/app/Hotel.js
+++ FrontEndWizcation/scripts/app/Hotel.js
@@ -1,1 +1,12 @@
-﻿
+﻿angular.module("Wizcation", []).controller("Hotel", function ($http, $scope) {
+    $scope.header = "kup";
+
+    $http.get("\api/moduleHotel/ListHotel/0").then(function (response) {
+        $scope.header = response.data;
+
+        $scope.ListHotel = $scope.header.hotels;
+
+        console.log(response.data);
+
+    });
+});
